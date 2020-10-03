2c5716d208b14ba78a9ec97964cce1ddcb20ba6c
RandyLu0 <33765989+RandyLu0@users.noreply.github.com>
2020-10-03 15:19:37 -0400

    Add files via upload

---
+++ euler.c
@@ -0,0 +1,23 @@
+#include "euler.h"
+int a;
+
+int problem1(){ 
+    int sum = 0;
+    for(a = 0; a < 1001; a++){
+        if(a % 3 == 0 || a % 5 == 0){
+            sum += a;
+        }
+    }
+    return sum;
+}
+
+int problem5(){ 
+    int squares = 0;
+    int squared = 0;
+    for(a = 0; a < 101; a++){
+        squared += a;
+        squares += a * a;
+    }
+    squared *= squared;
+    return squared - squares;
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "euler.h"
+
+int main(){
+    printf("%d\n",problem1());
+    printf("%d",problem6());
+    return 0;
+}
